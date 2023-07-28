3e289bca87c75da72334b6efa5a4f92d188a9d7f
minemobs <issahakim94@gmail.com>
2023-07-28 17:40:37 +0200

    Added code block component

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()]
+  integrations: [mdx(), tailwind()],
+  scopedStyleStrategy: 'class',
+  vite: {
+    assetsInclude: ['**/*.kt']
+  }
 });
