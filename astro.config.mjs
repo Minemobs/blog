b68bedfc99ece2e0ce84a31ffea548514103ddc5
minemobs <issahakim94@gmail.com>
2023-07-29 16:51:29 +0200

    Added prefetch,astroImageTools and compress plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-
 import node from "@astrojs/node";
+import { astroImageTools } from "astro-imagetools";
+import prefetch from "@astrojs/prefetch";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()],
+  integrations: [mdx(), tailwind(), prefetch(), astroImageTools, compress()],
   scopedStyleStrategy: 'class',
   vite: {
     assetsInclude: ['**/*.kt']
